22fa10a98e3a8d10f89ecdeaa72892aa7e60da3b
nicou08 <nicolas.guasca16@gmail.com>
2023-06-07 01:19:36 -0600

    Updated Projects and Resume

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -3,29 +3,46 @@ import classes from "./Projects.module.css";
 import ProjectContainer from "../layout/ProjectContainer";
 
 const Projects = () => {
+  const project0 = "Developed a full stack website using NextJS, Tailwind, and MongoDB. Currently the website is deployed on Vercel. The purpose of the website is to inform users about common and exotic pets.";
+  const project0Name = "Pet Information Website";
+  const project0Github = "https://github.com/nicou08/pet-app";
+  const project0Link = "https://pet-app-theta.vercel.app/";
+
   const project1 =
     "Developed a RESTful API of a stadium management system using the following techonlogies: Node.js, Express.js, Microsoft SQL Server hosted on AWS, and Postman.";
   const project1Name = "Stadium management system";
-  const project1Link = "https://github.com/hitony7/CPSC471_API";
+  const project1Github = "https://github.com/hitony7/CPSC471_API";
+  const project1Link = "";
 
   const project2 =
     "Develped a website that allowed an administrator to set-up a questionnaire in a specific topic and analyze the results witht the following technologies: ASP.NET Core MVC, Microsoft SQL Server on Azure, and Google Pie Charts.";
   const project2Name = "Questionnare Website";
-  const project2Link = "https://github.com/bryanlam549/CPSC571Project6";
+  const project2Github = "https://github.com/bryanlam549/CPSC571Project6";
+  const project2Link = "";
 
   const project3 =
     "Created the UI of a mobile app idea named 'Ordine'. The app allows users to make resarvations and pre-order food at restaurants. Created various prototypes using technologies such as Figma.";
   const project3Name = "Ordering and Reservation Mobile App UI/UX";
+  const project3Github = "https://github.com/cloudyyoung/ordine-ui-ux";
   const project3Link = "https://cloudyyoung.github.io/ordine-ui-ux/";
 
   return (
     <div className={classes.container}>
       <span className={classes.arrow}></span>
       <h1 className={classes.h1}>Projects</h1>
+      <div className={classes.p0}>
+        <ProjectContainer
+          projectName={project0Name}
+          projectInfo={project0}
+          proGit={project0Github}
+          proLink={project0Link}
+        />
+      </div>
       <div className={classes.p1}>
         <ProjectContainer
           projectName={project1Name}
           projectInfo={project1}
+          proGit={project1Github}
           proLink={project1Link}
         />
       </div>
@@ -33,6 +50,7 @@ const Projects = () => {
         <ProjectContainer
           projectName={project2Name}
           projectInfo={project2}
+          proGit={project2Github}
           proLink={project2Link}
         />
       </div>
@@ -40,6 +58,7 @@ const Projects = () => {
         <ProjectContainer
           projectName={project3Name}
           projectInfo={project3}
+          proGit={project3Github}
           proLink={project3Link}
         />
       </div>
--- src/components/layout/ProjectContainer.jsx
+++ src/components/layout/ProjectContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./ProjectContainer.module.css";
 import githubLogo from "../../assets/github-mark-white.png";
+import externalLink from "../../assets/external_link_icon_1.png";
 
 const ProjectContainer = (props) => {
   return (
@@ -9,8 +10,15 @@ const ProjectContainer = (props) => {
       <div className={classes.r1}></div>
       <div className={classes.pName}>{props.projectName}</div>
       <div className={classes.info}>{props.projectInfo}</div>
-      <a href={props.proLink} target="_blank">
-        <img src={githubLogo} alt="github logo" className={classes.proLink} />
+      {props.proLink === "" ? (
+        null 
+      ) : (
+        <a href={props.proLink} target="_blank">
+          <img src={externalLink} alt="external link" className={classes.proLink} />
+        </a>
+      )}
+      <a href={props.proGit} target="_blank">
+        <img src={githubLogo} alt="github logo" className={classes.proGit} />
       </a>
     </div>
   );
